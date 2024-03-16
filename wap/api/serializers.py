1a72450eef15ca81c2bd36e381caa8b93e8781d8
Keerthana Natarajan <113254958+keerthanajn@users.noreply.github.com>
2024-03-16 22:33:25 +0000

    Delete wap directory

--- wap/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from db.models import *
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
